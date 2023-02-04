52cb1d29ec2b63b7802972eb9d2c7be8444f36ef
Rowan Davitt <rowan.davitt1@ucalgary.ca>
2023-02-04 14:38:09 -0700

    added health and made enemies disapear if they reach the end

--- scripts/the_route.gd
+++ scripts/the_route.gd
@@ -1,21 +1,31 @@
 extends Node2D
 
 @export var wait = 1
-@export var speed = 300
+var health = 200
+var format = "Health: %s"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.wait_time = wait
 	$Timer.start()
+	$Label.text = format % health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for _i in $Path2D.get_children():
-		_i.progress += speed*delta
+		_i.progress += _i.speed*delta
 		_i.global_rotation = 0
+		
+		if (_i.progress_ratio >= 1.0):
+			_i.queue_free()
+			health -= _i.damage
+			$Label.text = format % health
 
 func _on_timer_timeout():
 	var spawn = preload("res://objects/character.tscn").instantiate()
 	$Path2D.add_child(spawn)
+	spawn.loop = false
+	
+	
