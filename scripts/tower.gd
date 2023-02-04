ec3eb3c88388fcb616ff231f5029e268aa487ef8
David Garcia <David.GarOro@protonmail.com>
2023-02-04 16:15:24 -0700

    Add enemy in range list for towers

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -3,8 +3,11 @@ extends Node2D
 const GHOST_ALPHA = 0.8
 
 var _can_be_placed: bool = true
+var _enemies_in_range: Dictionary = {}
 
-@onready var area = $Area2D
+@onready var place_area = $PlaceArea
+@onready var shoot_area = $ShootArea
+@onready var shoot_area_shape = $ShootArea/CollisionShape2D
 @onready var sprite = $Sprite2D
 @onready var _being_placed: bool = true
 
@@ -22,16 +25,22 @@ func _physics_process(delta: float) -> void:
 
 func place() -> void:
 	if _can_be_placed:
-		area.monitoring = false
-		area.monitorable = true
+		place_area.monitoring = false
+		place_area.monitorable = true
+		shoot_area_shape.disabled = false
 		sprite.modulate = Color(1,1,1,1)
 		_being_placed = false
 
-func _on_area_2d_area_entered(area):
+func _on_place_area_entered(_area: Area2D) -> void:
 	sprite.modulate = Color(1,0,0,GHOST_ALPHA)
 	_can_be_placed = false
 
-
-func _on_area_2d_area_exited(area):
+func _on_place_area_exited(_area: Area2D) -> void:
 	sprite.modulate = Color(1,1,1,GHOST_ALPHA)
 	_can_be_placed = true
+
+func _on_shoot_area_entered(area):
+	_enemies_in_range[area.get_parent().name] = area.get_parent()
+
+func _on_shoot_area_exited(area):
+	_enemies_in_range.erase(area.get_parent().name)
