683906910cc2c71ab749f2c401f217de01fbd21b
David Garcia <David.GarOro@protonmail.com>
2023-02-04 15:06:47 -0700

    Add coloring to ghost towers

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+const GHOST_ALPHA = 0.8
+
 var _can_be_placed: bool = true
 
+@onready var area = $Area2D
+@onready var sprite = $Sprite2D
 @onready var _being_placed: bool = true
+
+func _ready():
+	sprite.modulate = Color(1,1,1,GHOST_ALPHA)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_released("interact"):
@@ -15,11 +22,16 @@ func _physics_process(delta: float) -> void:
 
 func place() -> void:
 	if _can_be_placed:
+		area.monitoring = false
+		area.monitorable = true
+		sprite.modulate = Color(1,1,1,1)
 		_being_placed = false
 
 func _on_area_2d_area_entered(area):
+	sprite.modulate = Color(1,0,0,GHOST_ALPHA)
 	_can_be_placed = false
 
 
 func _on_area_2d_area_exited(area):
+	sprite.modulate = Color(1,1,1,GHOST_ALPHA)
 	_can_be_placed = true
