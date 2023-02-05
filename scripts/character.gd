04c62446a399df58624f02ed15bafc06977051a3
Rowan Davitt <rowan.davitt1@ucalgary.ca>
2023-02-04 17:54:15 -0700

    dded aphid and added health variable

--- scripts/character.gd
+++ scripts/character.gd
@@ -2,3 +2,4 @@ extends PathFollow2D
 
 @export var damage = 5
 @export var speed = 300
+@export var health = 20
--- scripts/the_route.gd
+++ scripts/the_route.gd
@@ -3,7 +3,8 @@ extends Node2D
 @export var wait = 1
 @export var health = 200
 var format = "Health: %s"
-var slug_spawn = false
+var spawn_type: Array[String] = ["res://objects/slug.tscn", "res://objects/ant.tscn", "res://objects/aphid.tscn"]
+var spawn_index = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +17,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for _i in $Path2D.get_children():
-		var old_location = _i.global_position
 		_i.progress += _i.speed*delta
 		
 		if (_i.progress_ratio >= 1.0):
@@ -26,15 +26,8 @@ func _process(delta):
 
 func _on_timer_timeout():
 	var spawn
-	if (slug_spawn):
-		spawn = preload("res://objects/slug.tscn").instantiate()
-		slug_spawn = false
-	else:
-		spawn = preload("res://objects/ant.tscn").instantiate()
-		slug_spawn = true
-		
-	spawn.loop = false
-	#spawn.rotates = false
+	spawn = load(spawn_type[spawn_index % spawn_type.size()]).instantiate()
+	spawn_index += 1
 	$Path2D.add_child(spawn)
 	
 	
